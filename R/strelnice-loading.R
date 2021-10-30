e36fa0ed08556b440ab2ab9b5866997ad6a30e09
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-10-30 02:49:12 +0200

    Fixes incorrect parameter loading

--- R/strelnice-loading.R
+++ R/strelnice-loading.R
@@ -7,28 +7,32 @@
 #'
 #' @examples
 load_strelnice_results <- function(filepth){
-  participant <- parse_participant(filepth)
-  timestamp <- parse_timestamp(filepth)
   log <- brainvr.reader::load_brainvr_log(filepth, quote="")
   df_log <- preprocess_results_log(log$data)
   stats <- data.frame()
   params <- data.frame()
-  for(i in 1:nrow(df_log)){
+  for(i in seq_len(nrow(df_log))){
     type <- df_log[i, "Event"]
-    if(type == "leveStart"){
-      out <- jsonlite::fromJSON(df_log[i, "Results"])
-      out$timestamp <- timestamp
-      out$participant <- participant
+    out <- jsonlite::fromJSON(df_log[i, "Results"])
+    out$timestamp <- log$session_header$Timestamp
+    out$participant <- log$session_header$Participant
+    out <- sapply(out, convert_vector_to_string,
+                  USE.NAMES = TRUE, simplify = FALSE)
+    if(type == "levelStart"){
       params <- rbind(params, out)
     }
     if(type == "levelEnd"){
-      out <- jsonlite::fromJSON(df_log[i, "Results"])
-      out$timestamp <- timestamp
-      out$participant <- participant
       stats <- rbind(stats, out)
     }
   }
   stats <- remove_percentages(stats)
   stats <- calculate_averages(stats)
   return(list(stats = stats, params = params))
+}
+
+convert_vector_to_string <- function(val){
+    val <- ifelse(length(val) > 1,
+                  paste0("[", paste0(val, collapse = ", "), "]"),
+                  val)
+    return(val)
 }
